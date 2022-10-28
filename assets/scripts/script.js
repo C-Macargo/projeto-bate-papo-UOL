9f19e8d08a6e8521827aeba0bb68881cd5d808bd
C-Macargo <caio.camargos97@gmail.com>
2022-10-28 17:20:32 -0300

    layout + chat + mensagem

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,38 +1,100 @@
-let UserName = {name: ""};
-let Messeges = [];
+let Messages = [];
+let FinalMessege = ""
 
 
+const UserName = prompt("Digite o seu nome")
+const User = {name: UserName};
+
+function UserLogin(){
+    const PromisseUserName = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", User)
+    PromisseUserName.catch(UserLogin)
+}
+UserLogin()
+
+
+setInterval (function StayLogged(){
+    const PromisseStatus = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", User)
+    PromisseStatus.then(StayLogged)
+}, 5000);
+
+
+
+
+const Promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+Promisse.then(ValidResponse)
 
 setInterval (function FetchChat(){
-    
+
     const Promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
     Promisse.then(ValidResponse)
-    RenderChat();
     }, 3000);
 
 function ValidResponse(response){
-    Messeges = response.data;
+    Messages = response.data;
+    RenderChat();
+    
 }
+
+
+
 
 
 
 function RenderChat(){
-    const MessegesList = document.querySelector('.content-container');
-    MessegesList.innerHTML="";
-    for (let i = 0; i < Messeges.length; i++){
-        let ChosenMessege = Messeges[i]
-        MessegesList.innerHTML += `            
-        <li class="messege-text ${ChosenMessege.type}">
-        <span>(${ChosenMessege.time})</span> ${ChosenMessege.from} para  ${ChosenMessege.to} : ${ChosenMessege.text}
-        <li>
-        
-    `;
+
+
+    const MessagesList = document.querySelector('.content-container');
+    MessagesList.lastElementChild.scrollIntoView();
+    MessagesList.innerHTML="";
+
+    for (let i = 0; i < Messages.length; i++){
+        let ChosenMessage = Messages[i]
+
+        if (ChosenMessage.type === "status"){
+            MessagesList.innerHTML += `            
+            <li class="messege-text ${ChosenMessage.type}">
+                <span>(${ChosenMessage.time})</span> <strong>${ChosenMessage.from}</strong> ${ChosenMessage.text}
+            <li>
+            `;
+        }
+        else if (ChosenMessage.type === "message"){
+            MessagesList.innerHTML += `            
+            <li class="messege-text ${ChosenMessage.type}">
+                <span>(${ChosenMessage.time})</span> <strong>${ChosenMessage.from}</strong> para  <strong>${ChosenMessage.to}</strong> : ${ChosenMessage.text}
+            <li>
+            `;
+        }
+        else if(ChosenMessage.type === "private_message"){
+                if (ChosenMessage.to == UserName){
+            MessagesList.innerHTML += `            
+            <li class="messege-text ${ChosenMessage.type}">
+                <span>(${ChosenMessage.time})</span> <strong>${ChosenMessage.from}</strong> para  <strong>${ChosenMessage.to}</strong> : ${ChosenMessage.text}
+            <li>
+            `;
+        }
+        }
     }
+    }
+    
+
+function SendMessage(){
+    let MessageText = document.querySelector("input").value
+
+
+    let SentMessage = { 
+        from: UserName,
+        to: "Todos",
+        text: MessageText,
+        type: "message"
+    }
+    const PromisseMessageSent = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",SentMessage)
+    document.querySelector("input").value=""
 }
 
-function UserLogin(){
-    UserName = prompt("Digite o seu nome")
-    
-}
+
+
+
+
+
 
 
